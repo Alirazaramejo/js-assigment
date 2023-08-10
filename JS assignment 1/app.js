4d40e078fd80b22d92f705d84ffabffd0b017fdf
Ali Raza Ramejo <125426009+Alirazaramejo@users.noreply.github.com>
2023-08-10 12:28:56 +0500

    Update app.js

--- JS assignment 1/app.js
+++ JS assignment 1/app.js
@@ -29,3 +29,31 @@ alert (" Welcome to JS Land...");
 
 
 alert ("Hello.. I can run JS through my web brower's console");
+
+
+
+//legal and nonlegal
+
+//Legal variable names:
+var myVar;
+var _myVar;
+var myVar123;
+var myVar_$;
+var $myVar;
+
+//Illegal variable names:
+
+var 123var; // Cannot start with a number
+var my-var; // Hyphens are not allowed
+var my var; // Spaces are not allowed
+var my#var; // Special characters other than $ and _ are not allowed
+var for; // Reserved keywords are not allowed
+
+
+//Display the rules for naming JS variables:
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, numbers, underscores, and dollar signs. For example: $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore (_) or dollar sign ($). For example: $name, _name or name<br>");
+document.write("Variable names are case-sensitive<br>");
+document.write("Variable names should not be JS reserved keywords<br>");
